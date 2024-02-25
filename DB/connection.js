b3b6932c2e9040373c09d60bb366851343d3c442
amaged1896 <ahmed.maged1896@gmail.com>
2024-02-25 02:23:17 +0200

    deploy demo onrender

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
-export const connection = () => {
-    mongoose.connect('mongodb://127.0.0.1:27017/e-commerce-api')
+export const connection = async () => {
+    // mongoose.connect('mongodb://127.0.0.1:27017/e-commerce-api')
+    await mongoose.connect(process.env.CLOUD_DATABASE)
         .then(() => console.log('Database connection established successfully'))
         .catch((err) => console.log('Database connection failed', err.message));
 }; 
